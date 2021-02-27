01c321e651bcd0b713859c783e621b35cc11f077
Marie Vaugoyeau <marie.vaugoyeau@gmail.com>
2021-02-27 15:12:04 +0100

    # je ne comprends pas pourquoi cela s'affiche dans la console et non sur la page Shiny....

--- test_app.R
+++ test_app.R
@@ -9,28 +9,45 @@ ui <- shinyUI(
     numericInput("hauteur_tricot", "Hauteur du tricot à réaliser en cm", value = 20, width = "400px", type = "small"),
     numericInput("largeur_tricot", "Largeur du tricot à réaliser en cm", value = 20, width = "400px", type = "small"),
     # numericInput("poids_echantillon", "Poids de l'échantillon de 10 * 10 cm", value = 10, width = "400px", type = "small"),
+    action_button("tricotage_image", "Créer l'image à tricoter"),
     h3("Image à tricoter"),
-    imageOutput("image_a_tricoter")
+    plotOutput("image_a_tricoter")#,
+    # h3("Instruction à télécharger"),
+    # action_button("image_telechargee", "Télécharger la grille")
   )
 )
 
 server <- shinyServer(function(input, output, session) {
-  output$image_a_tricoter <- renderImage({
-    if (is.null(input$image_choisi)) return("Pas d'image sélectionnée")
-    tricot::knitting_image(
-      tricot::image_load(
-        input$image_choisi
-      ), 
-      tricot::grid_size(
-        input$hauteur_tricot, 
-        input$largeur_tricot, 
-        tricot::square_size(
-          input$nbr_rang_10cm,
-          input$nbr_maille_10cm
+  
+  observeEvent(
+    input$tricotage_image, 
+    {
+      output$image_a_tricoter <- renderPlot({
+        plotly::ggplotly(
+          tricot::knitting_image(
+            tricot::image_load(
+              input$image_choisi$datapath
+            ), 
+            tricot::grid_size(
+              input$hauteur_tricot, 
+              input$largeur_tricot, 
+              tricot::square_size(
+                input$nbr_rang_10cm,
+                input$nbr_maille_10cm
+              )
+            )
+          )
         )
-      )
-    )
-  })
+      })
+    }
+    
+  )
+  
+  
+  # observeEvent(input$image_telechargee, {
+  #   
+  # })
+  
 })
 
 shiny::shinyApp(ui, server)
