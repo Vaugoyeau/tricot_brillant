02d2795f0db8d7015742df67ec72096ae00905cd
Marie Vaugoyeau <marie.vaugoyeau@gmail.com>
2021-03-03 11:14:23 +0100

    # modification pour déployer

--- app.R
+++ app.R
@@ -12,8 +12,8 @@ ui <- shinyUI(
          class = "header", 
          "Informations à remplir"
          ),
-        numericInput("nbr_rang_10cm", "Nombre de rangs pour l'échantillon de 10 cm", value = 25, width = "400px", type = "small"),
-        numericInput("nbr_maille_10cm", "Nombre de mailles pour l'échantillon de 10 cm", value = 20, width = "400px", type = "small"),
+        numericInput("nbr_rang_10cm", "Nombre de rangs pour l'échantillon (10 cm)", value = 25, width = "400px", type = "small"),
+        numericInput("nbr_maille_10cm", "Nombre de mailles pour l'échantillon (10 cm)", value = 20, width = "400px", type = "small"),
         numericInput("hauteur_tricot", "Hauteur du tricot à réaliser en cm", value = 20, width = "400px", type = "small"),
         numericInput("largeur_tricot", "Largeur du tricot à réaliser en cm", value = 20, width = "400px", type = "small"),
         # numericInput("poids_echantillon", "Poids de l'échantillon de 10 * 10 cm", value = 10, width = "400px", type = "small"),
---
+++ global.R
@@ -0,0 +1,5 @@
+### Fonctions utilisées développées dans le package {Tricot}
+
+
+devtools::install_github("Vaugoyeau/tricot")
+
